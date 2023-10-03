26b5fb1af03c3aea9d28d89860f030b009b0e80d
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2023-10-03 21:12:21 +0700

    Edit : Bullet Damage and Destroy Bullet

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -6,22 +6,24 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] int Damage = 10;
     [SerializeField] float Speed = 5f;
+    [SerializeField] float timerDead = 10f;
+    [SerializeField] float timer;
 
     void Update()
     {
+        timer += Time.deltaTime;
+        if(timer > timerDead)
+        {
+            Destroy(gameObject);
+        }
         transform.position += new Vector3(-Speed * Time.deltaTime, 0, 0);
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        /*if(col.TryGetComponent<PlayerHealth>(out var player))
+        if(col.tag == "PlayerHealth")
         {
-
-        }*/
-
-        if(col.tag == "Player")
-        {
-            Debug.Log("Shoot");
+            col.gameObject.GetComponent<PlayerHealth>().TakeDamage(Damage);
         }
 
         Destroy(gameObject);
