5036e781e155577df7d2b5e7b23fde2c0404fa2c
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2023-12-09 20:31:35 +0700

    Edit : Animation attackbox enemy

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     [SerializeField] int Damage = 10;
+
+    public BoxCollider2D enemyAttack;
     
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "PlayerHealth")
         {
             col.gameObject.GetComponent<PlayerHealth>().TakeDamage(Damage);
-            Debug.Log("Player");
+            
         }
     }
+
+  
 }
