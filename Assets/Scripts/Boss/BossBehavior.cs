83acf33730afe6728608b7a1eac3f07dd3f52e42
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2023-12-10 23:14:27 +0700

    ADD : Boss Attack && BeamAnimation

--- Assets/Scripts/Boss/BossBehavior.cs
+++ Assets/Scripts/Boss/BossBehavior.cs
@@ -7,6 +7,8 @@ public class BossBehavior : MonoBehaviour
     public float timer;
     public bool inRange;
     public GameObject triggerArea;
+    public GameObject BeamHorizontal;
+    public GameObject SpawnBeam;
     
     private float inTimer;
     private int AttackBoss = 1;
@@ -34,7 +36,12 @@ public class BossBehavior : MonoBehaviour
     {
         if(!cooling && AttackBoss == 1)
         {
-            Attack01();
+            Attack01_Left();
+        }
+
+        if(!cooling && AttackBoss == 2)
+        {
+            Attack01_Right();
         }
 
         if(cooling)
@@ -43,13 +50,24 @@ public class BossBehavior : MonoBehaviour
         }
     }
 
-    void Attack01()
+    void Attack01_Left()
     {
         timer = inTimer;
         cooling = true;
         AttackBoss = 2;
 
+        
         anim.SetTrigger("Attack01");
+    }
+
+    void Attack01_Right()
+    {
+        timer = inTimer;
+        cooling = true;
+        AttackBoss = 1;
+
+        Instantiate(BeamHorizontal, SpawnBeam.transform.position, Quaternion.identity);
+        anim.SetTrigger("Attack01_Right");
     }
 
     void Cooldown()
