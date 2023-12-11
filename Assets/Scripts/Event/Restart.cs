56fe5dca41620b063cdbde12fa8eac4bb510471d
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2023-12-11 15:17:15 +0700

    EDIT: Souls System (Reduced by Half Penalty when death)

--- Assets/Scripts/Event/Restart.cs
+++ Assets/Scripts/Event/Restart.cs
@@ -8,10 +8,12 @@ public class Restart : MonoBehaviour
     public SceneInfo sceneInfo;
     public VectorValue vectorValue;
     public Status status;
+    public Souls playerSouls;
     public void RestartGame()
     {
         status.CurrentHealth = status.MaxHealth;
         status.healQuantity = 3;
+        playerSouls.soul /= 2;
 
         SceneManager.LoadScene(sceneInfo.spawningScene, LoadSceneMode.Single);
         vectorValue.intialValue = sceneInfo.playerSpawnPosition;
