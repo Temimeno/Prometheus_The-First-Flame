0ffa0d1898c0f586e1f873b6b82922da0f51ad2f
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2023-12-12 03:46:45 +0700

    ADD : Animation Checkpoint

--- Assets/Scripts/Miscellaneous/Checkpoint.cs
+++ Assets/Scripts/Miscellaneous/Checkpoint.cs
@@ -8,10 +8,12 @@ public class Checkpoint : MonoBehaviour
 {
     private GameMaster gm;
     public SceneInfo sceneInfo;
+    private Animator anim;
 
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        anim = GetComponent<Animator>();
     }
 
     void OnTriggerEnter2D(Collider2D collider2D)
@@ -20,9 +22,16 @@ public class Checkpoint : MonoBehaviour
         {
             gm.lastCheckpointPos = transform.position;
             gm.checkPointAtScene = SceneManager.GetActiveScene().buildIndex;
+            anim.SetTrigger("OpenFire");
 
             sceneInfo.playerSpawnPosition = gm.lastCheckpointPos;
             sceneInfo.spawningScene = gm.checkPointAtScene;
         }
+    }
+
+    IEnumerator ChangeFire()
+    {
+        yield return new WaitForSeconds(2f);
+        anim.SetBool("Fire", true);
     }
 }
